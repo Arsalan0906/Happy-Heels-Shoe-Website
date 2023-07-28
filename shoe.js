dd76ef0df35d644fe16a0c000cc4a81f96999ab6
Arsalan0906 <139061161+Arsalan0906@users.noreply.github.com>
2023-07-28 19:29:47 +0530

    Add files via upload

---
+++ shoe.js
@@ -0,0 +1,25 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+
+
+
+
+const slider1 = document.querySelector('#glide_1');
+if (slider1) {
+  new Glide(slider1, {
+    type: 'carousel',
+    startAt: 0,
+    
+    gap: 0,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: 'linear',
+  }).mount();
+}
